68d2f499f1a4dcbc0d77d95fddf299ea76ac3521
Andrew Archbold <andrew.archbold@quiet.ly>
2016-11-28 17:53:42 -0800

    #time 1h 0m finishing desktop layout and adding table collapse toggle and in-page navigation

--- src/js/app/scripts.js
+++ src/js/app/scripts.js
@@ -21,8 +21,48 @@ $.fn.menuToggle = function(){
 
 }
 
+// mobile menu toggle
+$.fn.tableToggle = function(){
+    var container = $(this),
+        toggleBtn = $('.section-sales__table-toggle', container);
+
+    toggleBtn.click(function(e) {
+        e.preventDefault();
+        // fa-times fa-bars
+        if ($('.btn-sales',$(this)).hasClass('fa-plus')) {
+            $('.btn-sales',$(this)).removeClass('fa-plus');
+            $('.btn-sales',$(this)).addClass('fa-minus');
+            $(this).next('.section-sales__table').show();
+            $(this).addClass('-open');
+        } else {
+            $('.btn-sales',$(this)).removeClass('fa-minus');
+            $('.btn-sales',$(this)).addClass('fa-plus');
+            $(this).next('.section-sales__table').hide();
+            $(this).removeClass('-open');
+        }
+    })
+}
+
+// scroll to sections
+$.fn.scrolltoSection = function(){
+    var context = $(this),
+        body = $('html, body'),
+        links = $('a[data-goto]', context),
+        offset = 50;
+
+    links.click(function(e) {
+        e.preventDefault();
+        var target = $("section[data-target='" + $(this).data('goto') +"']");
+        setTimeout(function(){
+            body.stop(true, true).animate({ scrollTop: target.offset().top - offset }, 600);
+        }, 50);
+    })
+}
+
 $(function(){
     $('.-menu-bars').menuToggle();
+    $('.section-sales-container').tableToggle();
+    $('.section-intro__buttons').scrolltoSection();
     $('.carousel-now-selling').slick({
         // setting-name: setting-value
         infinite: true,
